5548fef78edff3d329b56a3576421ab46ee90cc1
hailong <zhenghailong@gmail.com>
2017-06-14 00:36:22 +0900

    add api and spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,8 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -38,6 +40,7 @@ end
 RSpec.configure do |config|
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
